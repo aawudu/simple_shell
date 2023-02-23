4ae126af2619f8181cb1973240bac08a76f9bdb5
Ampong Boadu <ampsboadu@gmail.com>
2023-02-22 19:24:56 -0800

    main func updated

--- main.c
+++ main.c
@@ -1,12 +1,49 @@
 #include "mainshell.h"
 
 /**
- * main -entry to shell program
- * Return: 0
+ *main - entry to shell
+ *@ac: number of args
+ *@av: array of args
+ *
+ *Return: 0 on success, 1 on error
  */
-
-int main()
+int main(int ac, char **av)
 {
-	printf("$ ");
-	return (0);
+	info_t info[] = { INFO_INIT
+	};
+
+	int fd = 2;
+
+	asm("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r"(fd)
+			: "r"(fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+
+			return (EXIT_FAILURE);
+		}
+
+		info->readfd = fd;
+	}
+
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
